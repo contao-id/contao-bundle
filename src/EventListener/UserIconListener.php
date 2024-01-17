08bf5e27eae2e9e3fa6f4d67775c80ba672ee672
Jim Schmid <jim.schmid@1up.io>
2024-01-17 08:25:29 +0100

    UserIconListener and user icons

---
+++ src/EventListener/UserIconListener.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ContaoId\ContaoBundle\EventListener;
+
+use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
+use Contao\DataContainer;
+use Contao\System;
+use Twig\Environment;
+
+#[AsCallback(table: 'tl_user', target: 'list.label.label')]
+class UserIconListener
+{
+    public function __construct(
+        private readonly Environment $twig,
+    ) {
+    }
+
+    public function __invoke(array $row, string $label, DataContainer $dataContainer, array $labels): array
+    {
+        /** @var \tl_user $tlUser */
+        $tlUser = System::importStatic('tl_user');
+        $labels = $tlUser->addIcon($row, $label, $dataContainer, $labels);
+
+        $labels[0] = $this->twig->render('@ContaoIdContao/user_icon.html.twig', [
+            'isAdmin' => !!$row['admin'],
+        ]);
+
+        return $labels;
+    }
+}
