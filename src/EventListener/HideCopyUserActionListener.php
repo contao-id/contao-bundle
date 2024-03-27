deeb94ad15e2da3126279e4ef937612289e590c9
David Greminger <david.greminger@1up.io>
2024-03-27 09:11:38 +0100

    Temporary re-add removed methods

--- src/EventListener/HideCopyUserActionListener.php
+++ src/EventListener/HideCopyUserActionListener.php
@@ -4,11 +4,21 @@ declare(strict_types=1);
 
 namespace ContaoId\ContaoBundle\EventListener;
 
+use Contao\Backend;
+use Contao\BackendUser;
 use Contao\DataContainer;
-use Contao\System;
+use Contao\Image;
+use Contao\StringUtil;
+use Symfony\Bundle\SecurityBundle\Security;
 
+// TODO: replace this listener with a proper voter
 class HideCopyUserActionListener
 {
+    public function __construct(
+        private readonly Security $security
+    ) {
+    }
+
     public function __invoke(
         array $row,
         ?string $href,
@@ -28,9 +38,12 @@ class HideCopyUserActionListener
             return '';
         }
 
-        /** @var \tl_user $tlUser */
-        $tlUser = System::importStatic('tl_user');
+        $user = $this->security->getUser();
 
-        return $tlUser->copyUser($row, (string) $href, $label, $title, (string) $icon, $attributes, $table);
+        if (!$user instanceof BackendUser) {
+            return '';
+        }
+
+        return ($user->isAdmin || !$row['admin']) ? '<a href="' . Backend::addToUrl($href . '&amp;id=' . $row['id']) . '" title="' . StringUtil::specialchars($title) . '"' . $attributes . '>' . Image::getHtml((string) $icon, $label) . '</a> ' : Image::getHtml(str_replace('.svg', '--disabled.svg', (string) $icon)) . ' ';
     }
 }
--- src/EventListener/HideToggleUserActionListener.php
+++ src/EventListener/HideToggleUserActionListener.php
@@ -4,11 +4,22 @@ declare(strict_types=1);
 
 namespace ContaoId\ContaoBundle\EventListener;
 
+use Contao\Backend;
+use Contao\BackendUser;
+use Contao\CoreBundle\Security\ContaoCorePermissions;
 use Contao\DataContainer;
-use Contao\System;
+use Contao\Image;
+use Contao\StringUtil;
+use Symfony\Bundle\SecurityBundle\Security;
 
+// TODO: replace this listener with a proper voter
 class HideToggleUserActionListener
 {
+    public function __construct(
+        private readonly Security $security
+    ) {
+    }
+
     public function __invoke(
         array $row,
         ?string $href,
@@ -28,9 +39,30 @@ class HideToggleUserActionListener
             return '';
         }
 
-        /** @var \tl_user $tlUser */
-        $tlUser = System::importStatic('tl_user');
+        // Check permissions AFTER checking the tid, so hacking attempts are logged
+        if (!$this->security->isGranted(ContaoCorePermissions::USER_CAN_EDIT_FIELD_OF_TABLE, 'tl_user::disable')) {
+            return '';
+        }
 
-        return $tlUser->toggleIcon($row, (string) $href, $label, $title, (string) $icon, $attributes);
+        $href .= '&amp;id=' . $row['id'];
+
+        if ($row['disable']) {
+            $icon = 'invisible.svg';
+        }
+
+        $user = $this->security->getUser();
+
+        if (!$user instanceof BackendUser) {
+            return '';
+        }
+
+        // Protect admin accounts and own account
+        if ((!$user->isAdmin && $row['admin']) || $user->id === $row['id']) {
+            return Image::getHtml((string) $icon) . ' ';
+        }
+
+        $titleDisabled = (\is_array($GLOBALS['TL_DCA']['tl_user']['list']['operations']['toggle']['label']) && isset($GLOBALS['TL_DCA']['tl_user']['list']['operations']['toggle']['label'][2])) ? sprintf($GLOBALS['TL_DCA']['tl_user']['list']['operations']['toggle']['label'][2], $row['id']) : $title;
+
+        return '<a href="' . Backend::addToUrl($href) . '" title="' . StringUtil::specialchars(!$row['disable'] ? $title : $titleDisabled) . '" data-title="' . StringUtil::specialchars($title) . '" data-title-disabled="' . StringUtil::specialchars($titleDisabled) . '" data-action="contao--scroll-offset#store" onclick="return AjaxRequest.toggleField(this,true)">' . Image::getHtml((string) $icon, $label, 'data-icon="visible.svg" data-icon-disabled="invisible.svg" data-state="' . ($row['disable'] ? 0 : 1) . '"') . '</a> ';
     }
 }
