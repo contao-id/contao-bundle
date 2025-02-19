b4d6cabbaabee519b8c7eb3d3e743a41df6bcb0a
David Greminger <david.greminger@1up.io>
2025-02-19 16:40:33 +0100

    Don't run old template listener on Contao 5.5+

--- src/EventListener/ButtonInjectionListener.php
+++ src/EventListener/ButtonInjectionListener.php
@@ -15,6 +15,10 @@ class ButtonInjectionListener
 
     public function __invoke(string $buffer, string $template): string
     {
+        if (class_exists(\Contao\CoreBundle\EventListener\Menu\BackendLoginListener::class)) {
+            return $buffer;
+        }
+
         if ('be_login' !== $template) {
             return $buffer;
         }
--- tests/EventListener/ButtonInjectionListenerTest.php
+++ tests/EventListener/ButtonInjectionListenerTest.php
@@ -10,6 +10,19 @@ use Twig\Environment;
 
 class ButtonInjectionListenerTest extends TestCase
 {
+    public function testDoesNothingInContao55Plus(): void
+    {
+        if ($this->isContao55OrNewer()) {
+            $twig = $this->createMock(Environment::class);
+            $twig->expects($this->never())->method('render');
+
+            $listener = new ButtonInjectionListener($twig);
+            $listener('_buffer', '_template');
+        } else {
+            $this->testAppendsButtonsOnLoginTemplate();
+        }
+    }
+
     public function testDoesNothingOnWrongTemplate(): void
     {
         $twig = $this->createMock(Environment::class);
@@ -22,14 +35,23 @@ class ButtonInjectionListenerTest extends TestCase
     public function testAppendsButtonsOnLoginTemplate(): void
     {
         $buffer = '<main><form><div>Hello!</div></form></main>';
-        $expectedBuffer = '<main><form><div>Hello!_buttons</div></form></main>';
+        $expectedBuffer = \sprintf('<main><form><div>Hello!%s</div></form></main>', $this->isContao55OrNewer() ? '' : '_buttons');
 
         $twig = $this->createMock(Environment::class);
-        $twig->expects($this->once())->method('render')->willReturn('_buttons');
+        $twig
+            ->expects($this->isContao55OrNewer() ? $this->never() : $this->once())
+            ->method('render')
+            ->willReturn('_buttons')
+        ;
 
         $listener = new ButtonInjectionListener($twig);
         $appended = $listener($buffer, 'be_login');
 
         $this->assertSame($expectedBuffer, $appended);
+    }
+
+    private function isContao55OrNewer(): bool
+    {
+        return class_exists(\Contao\CoreBundle\EventListener\Menu\BackendLoginListener::class);
     }
 }
