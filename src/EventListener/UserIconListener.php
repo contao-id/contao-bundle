4748d9dc18ac52409f199f4c140cbf2bfcc9f26f
David Greminger <bytehead@users.noreply.github.com>
2026-09-03 15:50:58 +0200

    Return `RecordLabel` if in Contao 6 context (#49)

--- src/EventListener/UserIconListener.php
+++ src/EventListener/UserIconListener.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace ContaoId\ContaoBundle\EventListener;
 
 use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\CoreBundle\DataContainer\RecordLabel;
 use Contao\DataContainer;
 use Contao\System;
 use Twig\Environment;
@@ -16,7 +17,9 @@ class UserIconListener
     ) {
     }
 
-    public function __invoke(array $row, string $label, DataContainer $dataContainer, array $labels): array
+    // TODO: Remove phpstan-ignore when dropping Contao 5 support
+    // @phpstan-ignore class.notFound
+    public function __invoke(array $row, string $label, DataContainer $dataContainer, array $labels): array|RecordLabel
     {
         /** @var \tl_user $tlUser */
         $tlUser = System::importStatic('tl_user');
@@ -26,10 +29,16 @@ class UserIconListener
             return $labels;
         }
 
-        $labels[0] = $this->twig->render('@ContaoIdContao/user_icon.html.twig', [
+        $html = $this->twig->render('@ContaoIdContao/user_icon.html.twig', [
             'isAdmin' => (bool) $row['admin'],
             'isLegacy' => version_compare(ContaoCoreBundle::getVersion(), '5.5.0', '<='),
         ]);
+
+        if (class_exists(RecordLabel::class)) {
+            return RecordLabel::fromHtml($html);
+        }
+
+        $labels[0] = $html;
 
         return $labels;
     }
