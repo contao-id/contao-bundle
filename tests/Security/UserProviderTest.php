26c0a32cf685f77c78c02c1f28e1c623a68ad7b4
David Greminger <bytehead@users.noreply.github.com>
2026-09-24 16:22:21 +0200

    Check remote id first before comparing email address (#57)

--- tests/Security/UserProviderTest.php
+++ tests/Security/UserProviderTest.php
@@ -162,6 +162,51 @@ class UserProviderTest extends TestCase
         $this->loadUser($connection, $logger, ['01a03400-d132-7321-92e0-8c2d0ccaa420']);
     }
 
+    public function testLinksAnUnlinkedLocalUserByEmail(): void
+    {
+        $connection = $this->mockConnection([], 'rick@example.com', false, 1);
+        $connection
+            ->expects($this->never())
+            ->method('insert')
+        ;
+
+        $connection
+            ->expects($this->once())
+            ->method('update')
+            ->with(
+                'tl_user',
+                $this->callback(static fn (array $data): bool => '01a03400-d132-7321-92e0-8c2d0ccaa420' === $data['contaoIdRemoteId']),
+                ['id' => 1],
+            )
+        ;
+
+        $logger = $this->createMock(LoggerInterface::class);
+
+        $this->loadUser($connection, $logger, ['01a03400-d132-7321-92e0-8c2d0ccaa420']);
+    }
+
+    public function testCreatesAUserIfNoneMatches(): void
+    {
+        $connection = $this->mockConnection([], 'rick@example.com', false, false);
+        $connection
+            ->method('lastInsertId')
+            ->willReturn('2')
+        ;
+
+        $connection
+            ->expects($this->once())
+            ->method('insert')
+            ->with(
+                'tl_user',
+                $this->callback(static fn (array $data): bool => '01a03400-d132-7321-92e0-8c2d0ccaa420' === $data['contaoIdRemoteId']),
+            )
+        ;
+
+        $logger = $this->createMock(LoggerInterface::class);
+
+        $this->loadUser($connection, $logger, ['01a03400-d132-7321-92e0-8c2d0ccaa420']);
+    }
+
     private function loadUser(Connection $connection, LoggerInterface $logger, array $clientUsers): UserInterface
     {
         $userProvider = new UserProvider($this->mockFramework(), $connection, $logger);
@@ -183,16 +228,22 @@ class UserProviderTest extends TestCase
         return $userProvider->loadUserByOAuthUserResponse($response);
     }
 
-    private function mockConnection(array $revokedUsers, false|string $username = 'rick@example.com'): Connection&MockObject
+    private function mockConnection(array $revokedUsers, false|string $username = 'rick@example.com', false|int $remoteIdMatch = 1, false|int $emailMatch = false): Connection&MockObject
     {
         $connection = $this->createMock(Connection::class);
         $connection
             ->method('executeQuery')
-            ->willReturnCallback(function (string $query) use ($username): Result {
+            ->willReturnCallback(function (string $query) use ($username, $remoteIdMatch, $emailMatch): Result {
                 $result = $this->createMock(Result::class);
 
+                if (str_starts_with($query, 'SELECT id FROM tl_user WHERE contaoIdRemoteId')) {
+                    $result->method('fetchOne')->willReturn($remoteIdMatch);
+                }
+
                 if (str_starts_with($query, 'SELECT id FROM tl_user WHERE email')) {
-                    $result->method('fetchOne')->willReturn(1);
+                    $this->assertStringContainsString("contaoIdRemoteId = ''", $query);
+
+                    $result->method('fetchOne')->willReturn($emailMatch);
                 }
 
                 if (str_starts_with($query, 'SELECT username FROM tl_user')) {
